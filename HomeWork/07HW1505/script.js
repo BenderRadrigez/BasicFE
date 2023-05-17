d3ae4c4889ee54814c0f7038fc01b20715d0836e
Andrei Petrusevich <111006214+BenderRadrigez@users.noreply.github.com>
2023-05-17 19:27:41 +0200

    Update script.js

--- HomeWork/07HW1505/script.js
+++ HomeWork/07HW1505/script.js
@@ -1,10 +1,10 @@
 // функция получает один аргумент (массив целых чисел)
 // возвращает сумму положительных элементов
-function summator_positive_elements(somearray){
+function summator_positive_elements(someArray){
     let sum = 0;
-    for (let i = 0; i < somearray.length; i++) {
-        if (somearray[i] > 0) {
-            sum += somearray[i];
+    for (let i = 0; i < someArray.length; i++) {
+        if (someArray[i] > 0) {
+            sum += someArray[i];
         }
     }
     return sum;
@@ -12,11 +12,11 @@ function summator_positive_elements(somearray){
 
 // функция которая получает один аргумент (массив целых чисел)
 // возвращает наибольшее число из массива.
-function max_number(somearray){
+function max_number(someArray){
     number = 0;
-    for(let i = 0; i < somearray.length; i++){
-        if (somearray[i] > number) {
-            number = somearray[i];
+    for(let i = 0; i < someArray.length; i++){
+        if (someArray[i] > number) {
+            number = someArray[i];
         }
     }
     return number;
